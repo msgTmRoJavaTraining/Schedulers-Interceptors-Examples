7d0e3b3697ff73fceadede592133ed98f3a3ac71
Andrei Astanei <andreihao@gmail.com>
2019-07-24 11:59:54 +0300

    Clasa pentru teste de functionare a exercitiilor

---
+++ src/main/test/java/group/msg/test/day13/MyTestClass.java
@@ -0,0 +1,36 @@
+package group.msg.test.day13;
+
+import group.msg.examples.day13.Employee;
+import group.msg.examples.day13.FirstExercise;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import java.util.logging.Logger;
+
+@RunWith(Arquillian.class)
+public class MyTestClass {
+    @Inject
+    private FirstExercise firstExercise;
+
+    @Inject
+    private Logger logger;
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "ISTExamples.war")
+                .addPackages(true, "group.msg")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+    @Test
+    public void testInterceptedHello() {
+        String response = firstExercise.getEmployeeNameForIntercept(new Employee("Astanei Andrei", "KFC"));
+        logger.info("Intercepted first exercise: " + response);
+        logger.info("================================================================");
+    }
+}
