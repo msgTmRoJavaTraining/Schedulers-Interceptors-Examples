c07bbb1fed07afc6a63733fb91786b8250e32915
Andrei Astanei <andreihao@gmail.com>
2019-07-24 12:00:34 +0300

    Clasa pentru obiectele folosite in exercitii

---
+++ src/main/java/group/msg/examples/day13/Employee.java
@@ -0,0 +1,13 @@
+package group.msg.examples.day13;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private String name;
+    private String favouriteFood;
+}
